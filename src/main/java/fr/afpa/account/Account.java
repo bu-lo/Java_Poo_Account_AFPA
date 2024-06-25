da1011e53fa2b96b2d50cdcc3399d6b835a6cb91
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-25 12:15:58 +0200

    Saving Part1 Class Account

--- src/main/java/fr/afpa/account/Account.java
+++ src/main/java/fr/afpa/account/Account.java
@@ -1,9 +1,89 @@
 package fr.afpa.account;
 
 /**
- * Classe représentant un compte bancaire
+ * Class representing a bank account
  */
-public class Account {    
-    // TODO à compléter
+public class Account {
+    private String iban;
+    private int balance;
+    private int overdraftAuthorization;
+
+    public Account(String iban, int balance, int overdraftAuthorization) {
+        this.iban = iban;
+        this.balance = balance;
+        this.overdraftAuthorization = overdraftAuthorization;
+    }
+
+    //METHODS
+
+    //ADDMONEY
+    public int addMoney(int amount) {
+        return this.balance = balance + amount;
+    }
+
+    //REMOVEMONEY
+    public int removeMoney(int amount) throws Exception{
+        if ((balance - amount) > overdraftAuthorization){
+            balance -= amount;
+        }else{
+            throw new Exception("ERROR: balance too low..");
+        } 
+        return balance;
+    }
+
+    //TRANSFER
+    public void transferMoney(int amount, Account debtorAccount, Account creditorAccount){
+        try {
+            debtorAccount.removeMoney(amount);
+            creditorAccount.addMoney(amount);
+        } catch (Exception e) {
+            System.out.println("ERROR: DEBTOR's balance too low..");
+        }
+    }
+
+    //CHECKIBAN
+    public boolean checkIban(String stringToCheck){
+        
+    }
+
+    // GETTERS & SETTERS
+    // IBAN
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    // BALANCE
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    // OVERDRAFTAUTHORIZATION
+    public int getOverdraftAuthorization() {
+        return overdraftAuthorization;
+    }
+
+    public void setOverdraftAuthorization(int overdraftAuthorization) {
+        this.overdraftAuthorization = overdraftAuthorization;
+    }
+
+
+    //TO STRING
+
+    @Override
+    public String toString(){
+        return "Account {"
+        + "IBAN: " + getIban()
+        + "| Balance: " + getBalance()
+        + "| Overdraft Authorization: " + getOverdraftAuthorization()
+        + "}";
+    }
 
 }
--- src/main/java/fr/afpa/account/AccountMain.java
+++ src/main/java/fr/afpa/account/AccountMain.java
@@ -11,6 +11,30 @@ class AccountMain
 
 		// TODO instancier des objets de la classe "Account"
 
+		Account account1 = new Account("FR7630001007941234567890185", 5000, -500);
+		Account account2 = new Account("FR7630001007941234567890186", 1000, 0 );
+
+		//TEST SUB AMOUNT ON ACCOUNT1
+		try{
+			account1.removeMoney(5000);
+		} catch (Exception exceptionToProcess){
+			System.out.println(exceptionToProcess.getMessage());
+		}
+
+		//TRANSFER MONEY BETWEEN ACCOUNT1 -> ACCOUNT2
+		transferMoney(6000, account1, account2); 
+		System.out.println("Balance account 1: " + account1.getBalance());
+		System.out.println("Balance account 2: " + account2.getBalance());
+
+
+		//TO STRING()
+		System.out.println(account1.toString());
+		System.out.println(account2.toString());
+
+
+
+
+
 		// TODO instancier des objets de la classe "Customer"
 		
 		// TODO ajouter un ou plusieurs objet de "Account" à des "Customer"
